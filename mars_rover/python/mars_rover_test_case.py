830a33412cd300cd2caa6167c3f9024cc66dbf36
yaelmd <72301354+yaelmd@users.noreply.github.com>
2022-11-23 21:49:56 +0100

    Finished

--- mars_rover/python/mars_rover_test_case.py
+++ mars_rover/python/mars_rover_test_case.py
@@ -20,19 +20,200 @@ Mars rover moves through
 
 
 class MarsRoverTestCase(unittest.TestCase):
-    def test_rover_move_forward(self):
+
+
+    def varios_movimientos(self):
+        rover = Rover(
+            start_x = 1,
+            start_y = 1,
+            orientation = 'N',
+            obstacles = [(0,1),(1,2),(0,0)]
+        )
+        movements = ['r','f','l','b','l','f']
+        expected_position = (1,0)
+        rover.move(movements)
+        self.assert_rover_position(expected_position,rover)
+
+    def test_rover_move_forward_obstacle(self):
+        rover = Rover(
+            start_x = 2,
+            start_y = 2,
+            orientation = 'N',
+            obstacles = [(0,1),(1,2)]
+        )
+        movements = ['b']
+        expected_position = (2,2)
+        rover.move(movements)
+        self.assert_rover_position(expected_position,rover)
+
+    def test_rover_move_forward_obstacle(self):
+        rover = Rover(
+            start_x = 0,
+            start_y = 0,
+            orientation = 'N',
+            obstacles = [(0,1),(1,2)]
+        )
+        movements = ['f']
+        expected_position = (0,0)
+        rover.move(movements)
+        self.assert_rover_position(expected_position,rover)
+
+
+    #Move backwards outside the borders
+
+    def test_rover_move_backwards_border(self):
+        rover = Rover(
+            start_x=0,
+            start_y=0,
+            orientation = 'N'
+        )
+        movements = ['b']
+        expected_position = (0,2)
+        rover.move(movements)
+
+        self.assert_rover_position(expected_position, rover)
+
+    def test_rover_move_backwards_east_border(self):
+        rover = Rover(
+            start_x=0,
+            start_y=2,
+            orientation = 'E'
+        )
+        movements = ['b']
+        expected_position = (2,2)
+        rover.move(movements)
+
+        self.assert_rover_position(expected_position, rover)
+
+
+    #Test move forward inside the borders
+    def test_rover_move_forward_south_border(self):
+        rover = Rover(
+            start_x=2,
+            start_y=0,
+            orientation='S'
+        )
+
+        movements = ['f']
+        expected_position = (2,2)
+        rover.move(movements)
+
+        self.assert_rover_position(expected_position, rover)
+    
+
+    def test_rover_move_forward_west_border(self):
+        rover = Rover(
+            start_x=0,
+            start_y=0,
+            orientation='W'
+        )
+
+        movements = ['f']
+        expected_position = (2,0)
+        rover.move(movements)
+
+        self.assert_rover_position(expected_position, rover)
+
+    #Move forward normal
+    def test_rover_move_forward_east(self):
+
+        rover = Rover(
+            start_x=1,
+            start_y=1,
+            orientation='E'
+        )
+
+        movements = ['f']
+        expected_position = (2, 1)
+        rover.move(movements)
+
+        self.assert_rover_position(expected_position, rover)
+
+    def test_rover_move_forward_south(self):
+        rover = Rover(
+            start_x=1,
+            start_y=1,
+            orientation='S'
+        )
+
+        movements = ['f']
+        expected_position = (1, 0)
+        rover.move(movements)
+
+        self.assert_rover_position(expected_position, rover)
+
+    
+    #Move backwards inside the borders
+    def test_rover_move_backwards_north(self):
         rover = Rover(
             start_x=1,
             start_y=1,
             orientation='N'
         )
 
-        movements = ['f']
-        expected_position = (1, 2)
+        movements = ['b']
+        expected_position = (1, 0)
         rover.move(movements)
 
         self.assert_rover_position(expected_position, rover)
 
+    def test_rover_move_backwards_east(self):
+        rover = Rover(
+            start_x=1,
+            start_y=1,
+            orientation='E'
+        )
+
+        movements = ['b']
+        expected_position = (0, 1)
+        rover.move(movements)
+
+        self.assert_rover_position(expected_position, rover)
+
+
+    #Turn right
+    def test_rover_turn_right(self):
+        rover = Rover(
+            start_x=1,
+            start_y=1,
+            orientation='N'
+        )
+
+        movements = ['r','r','r']
+        expected_orientation = 'W'
+        rover.move(movements)
+        self.assert_rover_orientation(expected_orientation, rover)
+        movements = ['r']
+        expected_orientation = 'N'
+        rover.move(movements)
+        self.assert_rover_orientation(expected_orientation, rover)
+
+
+    #Turn left
+    def test_rover_turn_left(self):
+        rover = Rover(
+            start_x=1,
+            start_y=1,
+            orientation='N'
+        )
+
+        movements = ['l','l','l']
+        expected_orientation = 'E'
+        rover.move(movements)
+        self.assert_rover_orientation(expected_orientation, rover)
+        movements = ['l']
+        expected_orientation = 'N'
+        rover.move(movements)
+        self.assert_rover_orientation(expected_orientation, rover)
+
+        
     def assert_rover_position(self, expected_position, rover):
         self.assertEqual(expected_position[0], rover.x)
         self.assertEqual(expected_position[1], rover.y)
+    
+    def assert_rover_orientation(self, expected_orientation, rover):
+        self.assertEqual(expected_orientation, rover.orientation)
+
+
+if __name__ == '__main__':
+    unittest.main()
--- mars_rover/python/rover.py
+++ mars_rover/python/rover.py
@@ -1,33 +1,65 @@
 class Rover(object):
 
+    DIRECTIONS = {'N':(0,1),'E':(1,0),'S':(0,-1),'W':(-1,0)}
+    ORIENTATIONS = {'N': ('E','W'),
+                    'E': ('S','N'),
+                    'S': ('W','E'),
+                    'W': ('N','S')}
+    MAP_X = 3
+    MAP_Y = 3
     x = 0
     y = 0
     orientation = 'N'
+    obstacles = []
+    
 
-    def __init__(self, start_x, start_y, orientation):
+    def __init__(self, start_x, start_y, orientation, obstacles=[]):
         self.x = start_x
         self.y = start_y
         self.orientation = orientation
+        self.obstacles = obstacles
 
     def move(self, movs):
         for m in movs:
-            if self.orientation == "N":
-                if m == "f":
-                    self.y = self.y + 1
+            try:
+                if m == 'f':
+                    self.move_forward()
+                elif m == 'b':
+                    self.move_backwards()
+                elif m == 'r':
+                    self.turn_right()
                 else:
-                    self.y = self.y - 1
-            elif self.orientation == "S":
-                if m == "f":
-                    self.y = self.y - 1
-                else:
-                    self.y = self.y + 1
-            elif self.orientation == "E":
-                if m == "f":
-                    self.x = self.x + 1
-                else:
-                    self.x = self.x - 1
-            elif self.orientation == "W":
-                if m == "f":
-                    self.x = self.x - 1
-                else:
-                    self.x = self.x + 1
+                    self.turn_left()
+            except Exception as e:
+                print(e)
+                break        
+
+    
+    def move_forward(self):
+        expected_x = (self.x + self.DIRECTIONS[self.orientation][0]) % self.MAP_X
+        expected_y = (self.y + self.DIRECTIONS[self.orientation][1]) % self.MAP_Y
+
+        if self.isObstacle(expected_x,expected_y):
+            raise Exception("There's an obstacle in your way")
+        else:
+            self.x = expected_x
+            self.y = expected_y
+
+    def move_backwards(self):
+        expected_x = (self.x - self.DIRECTIONS[self.orientation][0]) % self.MAP_X
+        expected_y = (self.y - self.DIRECTIONS[self.orientation][1]) % self.MAP_Y
+
+        if self.isObstacle(expected_x,expected_y):
+            raise Exception("There's an obstacle in your way")
+        else:
+            self.x = expected_x
+            self.y = expected_y
+
+    def turn_right(self):
+        self.orientation = self.ORIENTATIONS[self.orientation][0]
+
+    def turn_left(self):
+        self.orientation = self.ORIENTATIONS[self.orientation][1]
+
+    def isObstacle(self, x_rover, y_rover):
+        return (x_rover,y_rover) in self.obstacles
